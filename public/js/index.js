cc961b8fd01f3119c4e9bded81a1e7941da33a37
pb <pijush.barik8@gmail.com>
2019-01-17 22:09:45 +0530

    added gui form to send and receive messages

--- public/js/index.js
+++ public/js/index.js
@@ -5,9 +5,27 @@ socket.on('connect', function ()  {
 
   socket.on('newMessage', function (message) {
     console.log(message);
+    var li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    $('#messages').append(li);
   });
 
   socket.on('newUser', function (message) {
     console.log(message);
+    var newUser = $('<h4></h4>');
+    newUser.text(message.text);
+    $('#messages').append(newUser);
+  });
+});
+
+$('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'user',
+    text: $('#new-message').val()
+  }, function (response) {
+    console.log('received your message');
+    $('#new-message').val('');
   });
 });
