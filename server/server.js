dc5ad29912d0496cc6c79c7102d16f8f27023226
pb <pijush.barik8@gmail.com>
2019-01-17 15:34:46 +0530

    added connected & disconnected event listeners

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000;
 
-const publicPath = path.join(__dirname, '../public');
+const publicPath = path.join(__dirname, '../public');  // path to the public directory
 
 const app = express();
 
-app.use(express.static(publicPath));
+app.use(express.static(publicPath)); // set static public directory
 
-app.listen(port, () => {
+var server = app.listen(port, () => {
   console.log(`server is running on port ${port}...`);
+});
+
+const io = socketIO.listen(server); // bind socket.io to the server
+
+io.on('connection', socket => {
+  console.log('new user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
