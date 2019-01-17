d6c493192e13f09c186bca10e7d1eddd656e2eba
pb <pijush.barik8@gmail.com>
2019-01-17 23:11:32 +0530

    added test for generateLocationMessage

--- __tests__/message.test.js
+++ __tests__/message.test.js
@@ -1,4 +1,4 @@
-var { generateMessage } = require('./../server/utils/message');
+var { generateMessage, generateLocationMessage } = require('./../server/utils/message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -9,6 +9,26 @@ describe('generateMessage', () => {
 
     expect(message.from).toBe(from);
     expect(message.text).toBe(text);
+    expect(message.createdAt).toBeTruthy();
+    expect(typeof message.createdAt).toBe('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should return correct message object with location url', () => {
+    var from = 'tester';
+
+    var latitude = 20.211;
+    var longitude = 56.1412;
+
+    var message = generateLocationMessage(from, {
+      latitude,
+      longitude
+    });
+
+    expect(message.from).toBe(from);
+    expect(message.url).toBeTruthy();
+    expect(message.url).toBe(`https://www.google.com/maps/?q=${latitude},${longitude}`);
     expect(message.createdAt).toBeTruthy();
     expect(typeof message.createdAt).toBe('number');
   });
