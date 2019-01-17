1909ce7199a585562b2ad27d7ce504998dddffc2
pb <pijush.barik8@gmail.com>
2019-01-17 23:12:30 +0530

    added generateLocationMessage method

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,4 +1,4 @@
-var generateMessage = (from, text) => {
+const generateMessage = (from, text) => {
   return {
     from,
     text,
@@ -6,4 +6,12 @@ var generateMessage = (from, text) => {
   };
 };
 
-module.exports = { generateMessage };
+const generateLocationMessage = (from, cords) => {
+  return {
+    from,
+    url: `https://www.google.com/maps/?q=${cords.latitude},${cords.longitude}`,
+    createdAt: new Date().getTime()
+  }
+};
+
+module.exports = { generateMessage, generateLocationMessage };
