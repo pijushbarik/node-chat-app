9a4c8df4190233221aed76c10dc4b0922273aded
pb <pijush.barik8@gmail.com>
2019-01-17 16:35:36 +0530

    moved scripts from index.html to js/index.js file

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+var socket = io();
+
+socket.on('connect', function ()  {
+  console.log('connected to the server');
+
+  socket.on('newMessage', function (message) {
+    console.log(message);
+  });
+});
